d22f67a8bae0d2ad20e8e6bf8590f790355bdaeb
yacsaeedi <saeedi@dornica.net>
2021-04-17 13:44:23 +0430

    style tabBar

--- src/App/Navigator.js
+++ src/App/Navigator.js
@@ -2,20 +2,59 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { View } from 'react-native';
 import { Home, Cards, MoneyBox, Limits, Statistic } from "../Screens"
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 //Stack Names 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const TabScreen = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen options={{ headerShown: false }} name="Limits" component={Limits} />
-            <Tab.Screen options={{ headerShown: false }} name="MoneyBox" component={MoneyBox} />
-            <Tab.Screen options={{ headerShown: false }} name="Statistic" component={Statistic} />
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color }) => {
+                    const routeName = route.name;
+                    let iconName = routeName === "Home" ? "home" : routeName === "Statistic" ? "signal" : routeName === "MoneyBox" ? "database" : routeName === "Cards" ? "credit-card" : routeName === "Limits" ? "info-circle" : ""
+                    return (
+                        <View style={{
+                            width: '75%', height: '75%', alignSelf: 'center', justifyContent: "center", alignItems: "center",
+                            borderRadius: 15,
+                        }
+                        } >
+                            <Icon name={iconName} size={14} color="#8c8c8e" />
+                        </View >
+                    );
+                }
+
+            })}
+            tabBarOptions={{
+                labelStyle: {
+                    fontSize: 12,
+                    marginBottom: 10,
+                    color: "#707173",
+                },
+                style: {
+                    borderTopWidth: 0.5,
+                    borderTopColor: "#292c35",
+                    backgroundColor: "#17191F",
+                    elevation: 0,
+                    width: '100%',
+                    height: 60,
+                },
+
+            }}
+            backBehavior="none"
+            initialRouteName="Home"
+
+        >
             <Tab.Screen options={{ headerShown: false }} name="Home" component={Home} />
+            <Tab.Screen options={{ headerShown: false }} name="MoneyBox" component={MoneyBox} />
             <Tab.Screen options={{ headerShown: false }} name="Cards" component={Cards} />
+            <Tab.Screen options={{ headerShown: false }} name="Limits" component={Limits} />
+            <Tab.Screen options={{ headerShown: false }} name="Statistic" component={Statistic} />
         </Tab.Navigator>
     )
 }
